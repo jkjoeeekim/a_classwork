47335f9c18d65ad2b9850dd817fe10040f62d63d
Joseph Kim <jkjoeeekim@live.com>
2021-06-25 10:33:31 -0700

    add

--- W7D5/reddit_clone_app/app/controllers/sessions_controller.rb
+++ W7D5/reddit_clone_app/app/controllers/sessions_controller.rb
@@ -1,13 +1,20 @@
 class SessionsController < ApplicationController
   def new
-
+    render :new
   end
 
   def create
-
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login_user!(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = ["Invalid credentials"]
+      render :new
+    end
   end
 
   def destroy
-    
+    logout
   end
 end
--- W7D5/reddit_clone_app/app/controllers/users_controller.rb
+++ W7D5/reddit_clone_app/app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_logged_in, only: [:show, :edit, :update, :destroy, :index]
+  before_action :require_logged_out, only: [:create, :new] 
+
   def show
     @user = User.find(params[:id])
     render :show
@@ -31,11 +34,24 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find_by_credentials
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :edit
+    end
   end
 
   def destroy
-
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to new_session_url
+    # if @user.destroy
+    #   redirect_to users_url
+    # else
+    #   flash.now[:errors] = ['Cannot destroy this user']
+    # end
   end
 
   private
