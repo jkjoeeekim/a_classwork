d3e500a94a3a45b09c391fe78fa4cd82764d584a
Joseph Kim <jkjoeeekim@live.com>
2021-07-01 16:16:49 -0700

    Add absurdBubbleSort function

---
+++ W8D4/callbacks.js
@@ -0,0 +1,141 @@
+const { read } = require('fs');
+
+// Using Date() to create a simple clock
+class Clock {
+  constructor() {
+    this.date = new Date();
+    const [hours, minutes, seconds] = [this.date.getHours(), this.date.getMinutes(), this.date.getSeconds()];
+    this.time = {
+      hour: hours,
+      minute: minutes,
+      second: seconds
+    };
+    this.printTime();
+  }
+
+  printTime() {
+    console.log(`${this.time['hour']}h:${this.time['minute']}m:${this.time['second']}s`);
+  }
+
+  // using ES6 syntax, the scope of 'this'(clock object) persists through the arrow function.
+  _tick() {
+    setInterval(() => {
+      console.log(this);
+      this.time['second']++;
+      if (this.time['second'] === 60) {
+        this.time['second'] = 0;
+        this.time['minute']++;
+      };
+      if (this.time['minute'] === 60) {
+        this.time['minute'] = 0;
+        this.time['hour']++;
+      }
+      if (this.time['hour'] === 25) {
+        this.time['hour'] = 0;
+      }
+      this.printTime();
+    }, 1000);
+  }
+
+  // using ES5 syntax, the scope of 'this'(clock object) must be defined outside of setInterval().
+  _tick2() {
+    let that = this;
+    setInterval(function () {
+      console.log(that);
+      that.time['second']++;
+      if (that.time['second'] === 60) {
+        that.time['second'] = 0;
+        that.time['minute']++;
+      };
+      if (that.time['minute'] === 60) {
+        that.time['minute'] = 0;
+        that.time['hour']++;
+      }
+      if (that.time['hour'] === 25) {
+        that.time['hour'] = 0;
+      }
+      that.printTime();
+    }, 1000);
+  }
+}
+
+// const clock = new Clock();
+// clock._tick();
+// clock._tick2();
+
+
+// Using readline to get user prompt
+readline = require('readline');
+reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// simple adding numbers function using reader
+let addNumbers = (sum, numsLeft, completionCallback) => {
+  if (numsLeft > 0) {
+    console.log(`Current sum: ${sum}`);
+    reader.question('Enter a number to add: \n', (number) => {
+      sum += parseInt(number);
+      numsLeft--;
+      console.log(`New sum: ${sum}\n\n`);
+      addNumbers(sum, numsLeft, completionCallback);
+    });
+  } else {
+    reader.close();
+    completionCallback(sum);
+  }
+};
+
+// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+
+// my absurd bubble sort method using reader
+function askIfGreaterThan(el1, el2, callback) {
+  reader.question(`Is ${el1} greater than ${el2}?\n`, (answer) => {
+    if (answer === 'yes') {
+      callback(true);
+    } else if (answer === 'no') {
+      callback(false);
+    } else {
+      console.log('Not a valid response, answer yes or no');
+      askIfGreaterThan(el1, el2, callback);
+    }
+  });
+}
+
+let innerBubbleSortLoop = (arr, i, madeAnySwaps, outerBubbleSortLoop) => {
+  if (i === arr.length - 1) {
+    outerBubbleSortLoop(madeAnySwaps);
+  } else {
+    let first = arr[i];
+    let second = arr[i + 1];
+    let madeSwap;
+    askIfGreaterThan(first, second, bool => {
+      console.log(arr);
+      if (bool) {
+        madeSwap = bool;
+        arr[i] = second;
+        arr[i + 1] = first;
+      }
+      console.log(arr);
+      innerBubbleSortLoop(arr, i + 1, madeSwap, outerBubbleSortLoop);
+    });
+  }
+};
+
+let absurdBubbleSort = (arr, sortCompletionCallback) => {
+  function outerBubbleSortLoop(madeAnySwaps) {
+    if (madeAnySwaps) {
+      innerBubbleSortLoop(arr, 0, true, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback(arr);
+    }
+  }
+  outerBubbleSortLoop(true);
+};
+
+// absurdBubbleSort([4, 2, 1, 4, 3, 23, 5], function(arr) {
+//   console.log("Sorted array: " + JSON.stringify(arr));
+//   reader.close();
+// });
