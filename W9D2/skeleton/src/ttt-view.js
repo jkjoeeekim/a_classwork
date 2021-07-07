4a3a0dd99f6268f6bf2f5143600f20484555a1d3
Joseph Kim <jkjoeeekim@live.com>
2021-07-07 12:24:32 -0700

    Finish setupBoard function in View

--- W9D2/skeleton/src/ttt-view.js
+++ W9D2/skeleton/src/ttt-view.js
@@ -1,7 +1,18 @@
+const Board = require("../ttt_node/board.js")
+
 class View {
   constructor(game, el) {}
 
-  setupBoard() {}
+  setupBoard() {
+    const ul = document.createElement("ul");
+    for (let x = 0; x < 3; x++) {
+      for (let y = 0; y < 3; y++) {
+        const li = document.createElement("li");
+        li.dataset.position = JSON.stringify([x, y]);
+        ul.append(li);
+      }
+    }
+  }
   
   bindEvents() {}
 
@@ -10,5 +21,6 @@ class View {
   makeMove(square) {}
 
 }
-
+let v1 = new View
+v1.setupBoard();
 module.exports = View;
