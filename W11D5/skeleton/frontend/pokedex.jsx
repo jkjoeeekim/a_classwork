5e2ba95f213be7fb1ef3f28a98f462418560fa54
Joseph Kim <jkjoeeekim@live.com>
2021-07-25 15:12:46 -0700

    Add updated W11D5

--- W11D5/skeleton/frontend/pokedex.jsx
+++ W11D5/skeleton/frontend/pokedex.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as api_util from './util/api_util'
 import {receiveAllPokemon} from './actions/pokemon_actions'
+import configureStore from './store/store'
 
 document.addEventListener('DOMContentLoaded', () => {
     let root = document.getElementById('root');
-
+    // debugger;
     window.fetchAllPokemon = api_util.fetchAllPokemon;
     window.receiveAllPokemon = receiveAllPokemon;
+    window.store = configureStore;
+    // window.getState = store.getState();
+    // window.dispatch = store.dispatch();
 
     ReactDOM.render(<h1>Hello</h1>, root);
 });
